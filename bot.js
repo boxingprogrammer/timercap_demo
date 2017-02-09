66970a1e70ebf73331b5e9634fb18b066caf601d
Cliff Rayman <cliff@rayman.com>
2017-02-09 14:58:40 -0800

    get the main files saved

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 /*global require, module, console */
+const util = require('util');
 const botBuilder = require('claudia-bot-builder'),
 	  getIntentName = function (alexaPayload) {
 		  'use strict';
@@ -11,31 +12,60 @@ const botBuilder = require('claudia-bot-builder'),
 const api = botBuilder(
 	function (message, originalRequest) {
 		'use strict';
-		console.log(originalRequest.body);
-		// message.text has all intent placeholders joined together, for quick access
-		if (message.text) {
-			 // just return a text message to have it automatically packaged
-			 // as a PlainText Alexa response, continuing the session
-			 return 'Spelling bee says ' + message.text + ' is spelled ' + message.text.toUpperCase().split('').join('. ') + '.';
-		// you can use all the Alexa request properties from originalRequest.body
-		} else if (getIntentName(originalRequest.body) === 'ExitApp'){
-			// return a JavaScript object to set advanced response params
-			// this prevents any packaging from bot builder and is just
-			// returned to Alexa as you specify
-			return {
-				response: {
-					outputSpeech: {
-						type: 'PlainText',
-						text: 'Bye from Spelling Bee!'
-					},
-					shouldEndSession: true
-				}
-			};
+	    console.log(originalRequest.body, false, null);
+	    console.log(message.text, false, null);
+	    if (getIntentName(originalRequest.body) === 'MedTaking'){
+		if(message.text){
+		    if(message.text.toLowerCase() === 'edarbi' || message.text.toLowerCase() === 'cialis'){
+			return 'Yes, you are taking ' + message.text;
+		    } else {
+			return 'No, you are not taking ' + message.text;
+		    }
 		} else {
-			return {};
+		    return 'You are currently taking Edarbi and Cialis';
 		}
+	    } else if (getIntentName(originalRequest.body) === 'MedDue'){
+		if(message.text){
+		    if(message.text.toLowerCase() === 'edarbi'){
+			return 'Your ' + message.text + ' is due at 8:00am and 10:00pm';
+		    } else if (message.text.toLowerCase() === 'cialis'){
+			return 'Your ' + message.text + ' is due at 8:00am';
+		    } else {
+			return 'I do not show that you are currently taking ' + message.text;
+		    }
+		} else {
+		    return 'Your Edarbi and Cialis are due at 8:00am and your Edarbi is due at 10:00pm';
+		}
+	    } else if (getIntentName(originalRequest.body) === 'MedTaken'){
+		if(message.txt){
+		    if(message.text.toLowerCase() === 'edarbi'){
+			return 'Your ' + message.text + ' was last taken at 8:11am';
+		    } else if (message.text.toLowerCase() === 'cialis'){
+			return 'Your ' + message.text + ' was last taken yesterday at 7:57am';
+		    } else {
+			return 'I do not show that you are currently taking ' + message.text;
+		    }
+		} else {
+		    return 'Your Edarbi was last taken at 8:11am and your Cialis was last taken yesterday at 7:57am';
+		}
+	    } else if (getIntentName(originalRequest.body) === 'ExitApp'){
+		// return a JavaScript object to set advanced response params
+		// this prevents any packaging from bot builder and is just
+		// returned to Alexa as you specify
+		return {
+			response: {
+				outputSpeech: {
+					type: 'PlainText',
+					text: 'Bye from Rayman first!'
+				},
+				shouldEndSession: true
+			}
+		};
+	    } else {
+		return 'Rayman first did not understand your request';
+	    }
 	},
-	{ platforms: ['alexa'] }
+    { platforms: ['alexa'] }
 );
 
 module.exports = api;
